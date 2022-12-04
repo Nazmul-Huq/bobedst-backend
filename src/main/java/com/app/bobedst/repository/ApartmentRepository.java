9a78ec96a2b7174e82fa6c65c6aed397525bba5f
Søs <slys87@gmail.com>
2022-12-04 18:14:05 +0100

    Apartment Ifo DB created

--- src/main/java/com/app/bobedst/repository/ApartmentRepository.java
+++ src/main/java/com/app/bobedst/repository/ApartmentRepository.java
@@ -1,7 +1,8 @@
 package com.app.bobedst.repository;
 
 import com.app.bobedst.model.Apartment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ApartmentRepository extends CrudRepository<Apartment, Long> {
+public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
 }
